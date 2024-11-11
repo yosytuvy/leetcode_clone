a7c3c25f9b9400490b9bf8e85355a5143aca3381
yosytuvy <yoseftoovian@gmail.com>
2024-11-11 23:34:09 +0200

    add firebase logic

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,14 +1,27 @@
 import Navbar from "@/components/Navbar/Navbar";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import AuthModal from "@/components/Modals/AuthModal";
 import { useRecoilValue } from "recoil";
 import { authModalState } from "@/atoms/AuthModalAtom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/firebase";
+import { useRouter } from "next/router";
 
 type AuthPageProps = {};
 
 const AuthPage: React.FC<AuthPageProps> = () => {
     const authModal = useRecoilValue(authModalState);
+    const [user, loading, error] = useAuthState(auth);
+    const [pageLoading, setPageLoading] = useState(true);
+    const router = useRouter();
+
+    useEffect(() => {
+        if (user) router.push("/");
+        if (!loading && !user) setPageLoading(false);
+    }, [user, router, loading]);
+
+    if (pageLoading) return null;
 
     return (
         <div className="bg-gradient-to-b from-gray-600 to-black h-screen relative">
